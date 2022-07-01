94e46ade0847835587b3e205218f019683df1094
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-06-30 19:59:54 -0500

    adjusted README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 6-30-notetaker
+
+**Description**
+This is an application that keeps notes and saves them in a list. This is done through an HTML page with a JavScript backend APIs.
+
+
+**Links**
+
+Pages:
+https://prsoulman.github.io/6-30-notetaker/
+
+**Technologies Used**
+-*HTML*
+-*CSS*
+-*JavaScript*
+-*node.js*
+
+
+
--- Routes/API.js
+++ Routes/API.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const fs = require('fs');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 const {v4:uuidv4} = require('uuid');
 
 const getNotes = () => {
@@ -16,15 +17,32 @@ const getNotes = () => {
     })
 }
 
+
 router.get('/api/notes',(req,res) => {
     getNotes().then(notes => res.json(notes))
 })
 
 router.post('/api/notes',(req,res) => {
-    getNotes().then(oldNotes => {
+    saveNotes().then(oldNotes => {
         console.log(req.body)
+        
     })
 })
+router.delete('/api/notes'),(req,res) => {
+    getNotes().then(notes => res.json(notes))
+}
+
+        // // Display index.html when all other routes are accessed
+        // app.get('*', (req,res) => {
+        //     res.sendFile(path.join(__dirname, "../public/index.html"));
+        // });
+
+        // //updates the json file whenever a note is added or deleted
+        // function updateDb() {
+        //     fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+        //         if (err) throw err;
+        //         return true;
+        //     });
 
 
 module.exports=router
